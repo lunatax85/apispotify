81e2efeedadb899fc35798a2bcfb71f0f752af99
andresbluna <andres.benjamin.luna@gmail.com>
2023-11-09 22:42:35 -0300

    Initial commit

---
+++ src/main/java/com/example/apispotify/ApispotifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.apispotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApispotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApispotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apispotify/ApispotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apispotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApispotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
